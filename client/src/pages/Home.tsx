364e4020e41d1a01f8bd2216e2fac60d9e3e77f6
chenxiang <chenxiangbj@kanyun.com>
2024-12-24 18:34:38 +0800

    增加 logo

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { Card, Button, Typography, Space, Tag } from "antd";
+import { Card, Button, Typography, Space, Tag, Image } from "antd";
 import {
   GithubOutlined,
   MailOutlined,
@@ -75,9 +75,18 @@ const Home: React.FC = () => {
   return (
     <div>
       <Card style={{ width: "100%", maxWidth: "800px" }}>
-        <Title level={2} style={{ textAlign: "center", marginBottom: "24px" }}>
-          邮件工具箱
-        </Title>
+        <Space direction="vertical" align="center" style={{ width: "100%" }}>
+          <Image
+            src="/logo.png"
+            alt="HTML Mail"
+            preview={false}
+            width={132}
+            height={132}
+          />
+          <Title level={2} style={{ margin: 0, marginBottom: "24px" }}>
+            邮件工具箱
+          </Title>
+        </Space>
 
         <Space direction="vertical" size="large" style={{ width: "100%" }}>
           {user ? (
@@ -126,11 +135,27 @@ const Home: React.FC = () => {
             </Card>
           )}
 
-          <Card type="inner" title="功能介绍">
-            <Space direction="vertical">
-              <Text>• 支持发送 HTML 格式的邮件</Text>
-              <Text>• 使用 GitHub 账号快速登录</Text>
-              <Text>• 简单易用的界面设计</Text>
+          <Card type="inner" title="设计初衷">
+            <Space direction="vertical" style={{ width: "100%" }}>
+              <Text>在开发 HTML 邮件时，我们经常遇到以下挑战：</Text>
+              <ul style={{ margin: "8px 0", paddingLeft: "20px" }}>
+                <li>
+                  <Text>各邮件客户端对 HTML 内容的渲染支持度参差不齐</Text>
+                </li>
+                <li>
+                  <Text>调试过程繁琐，需要不断发送测试邮件来验证效果</Text>
+                </li>
+                <li>
+                  <Text>市面上缺乏好用的 HTML 邮件编辑工具</Text>
+                </li>
+                <li>
+                  <Text>现有的在线服务多为海外付费产品，使用不便</Text>
+                </li>
+              </ul>
+              <Text>
+                基于以上原因，我们开发了这个简单的工具，希望能帮助开发者更方便地测试和调试
+                HTML 邮件。
+              </Text>
             </Space>
           </Card>
         </Space>
