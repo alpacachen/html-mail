74b4df6a519cd71fed8619dfb6faa99d7a220514
chenxiang <chenxiangbj@kanyun.com>
2024-12-29 15:37:19 +0800

    简历生成器 1

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,49 +1,20 @@
 import { Routes, Route } from "react-router-dom";
-import { Flex } from "antd";
 import HtmlMailRoutes from "./routes/html-mail";
 import NotFound from "./pages/not-found";
 import GithubCallback from "./pages/github-oauth-callback";
 import GiteeCallback from "./pages/gitee-oauth-callback";
-
-const AppLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => (
-  <div
-    style={{
-      minHeight: "100vh",
-      boxSizing: "border-box",
-      padding: "24px",
-      position: "relative",
-    }}
-  >
-    <Flex
-      style={{
-        width: "100%",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <div
-        style={{
-          width: "100%",
-          maxWidth: "800px",
-        }}
-      >
-        {children}
-      </div>
-    </Flex>
-  </div>
-);
+import MarkdownResume from "./pages/markdown-resume";
 
 function App() {
   return (
-    <AppLayout>
-      <Routes>
-        <Route path="/" element={<div>1112</div>} />
-        <Route path="/html-mail/*" element={<HtmlMailRoutes />} />
-        <Route path="/auth/github/callback" element={<GithubCallback />} />
-        <Route path="/auth/gitee/callback" element={<GiteeCallback />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </AppLayout>
+    <Routes>
+      <Route path="/" element={<div>1112</div>} />
+      <Route path="/html-mail/*" element={<HtmlMailRoutes />} />
+      <Route path="/markdown-resume/*" element={<MarkdownResume />} />
+      <Route path="/auth/github/callback" element={<GithubCallback />} />
+      <Route path="/auth/gitee/callback" element={<GiteeCallback />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,60 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { ConfigProvider, theme } from 'antd'
 import './i18n'
 import App from './App.tsx'
 import './index.css'
 
+const customTheme = {
+  token: {
+    colorPrimary: '#000000',
+    colorInfo: '#000000',
+    colorBgContainer: '#ffffff',
+    colorBorder: '#d9d9d9',
+    colorText: '#262626',
+    colorTextSecondary: '#595959',
+    colorBgTextHover: '#f5f5f5',
+    colorBgTextActive: '#f0f0f0',
+    borderRadius: 2,
+    wireframe: true,
+  },
+  components: {
+    Button: {
+      colorPrimary: '#000000',
+      defaultBg: '#ffffff',
+      defaultBorderColor: '#d9d9d9',
+      defaultColor: '#262626',
+      primaryColor: '#ffffff',
+    },
+    Input: {
+      colorBorder: '#d9d9d9',
+      colorPrimaryHover: '#262626',
+      colorText: '#262626',
+      colorTextPlaceholder: '#8c8c8c',
+    },
+    Select: {
+      colorBgContainer: '#ffffff',
+      colorBorder: '#d9d9d9',
+      colorText: '#262626',
+      colorTextPlaceholder: '#8c8c8c',
+      optionSelectedBg: '#f5f5f5',
+    },
+    Card: {
+      colorBgContainer: '#ffffff',
+      colorBorderSecondary: '#f0f0f0',
+      colorText: '#262626',
+    },
+  },
+  algorithm: theme.defaultAlgorithm,
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ConfigProvider theme={customTheme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ConfigProvider>
   </React.StrictMode>,
 )
