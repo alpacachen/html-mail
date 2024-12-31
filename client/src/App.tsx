209f888902954c9c9abfdc3c6acbf22d6aad9569
chenxiang <chenxiangbj@kanyun.com>
2024-12-31 19:03:20 +0800

    删除所有简历生成器的代码

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,14 +3,12 @@ import HtmlMailRoutes from "./routes/html-mail";
 import NotFound from "./pages/not-found";
 import GithubCallback from "./pages/github-oauth-callback";
 import GiteeCallback from "./pages/gitee-oauth-callback";
-import MarkdownResume from "./pages/markdown-resume";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<div>1112</div>} />
       <Route path="/html-mail/*" element={<HtmlMailRoutes />} />
-      <Route path="/markdown-resume/*" element={<MarkdownResume />} />
       <Route path="/auth/github/callback" element={<GithubCallback />} />
       <Route path="/auth/gitee/callback" element={<GiteeCallback />} />
       <Route path="*" element={<NotFound />} />
